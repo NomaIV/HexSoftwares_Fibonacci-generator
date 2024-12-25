0f68112abeca4a7336a6a3499d00794df7357d48
Noma <ngwanenwj@gmail.com>
2024-12-25 17:18:17 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Fibonacci function for nth number
+def Fibonacci(n):
+    if n<=0:
+        print("Enter a positive number")
+        # First fibonacci number is 0
+    elif n ==1:
+        return 0
+        # Second fibonacci number is 1
+    elif n ==2:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+# Test the function    
+print(Fibonacci(11))
+
+
+
